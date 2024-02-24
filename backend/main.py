b2a903b24b67f2877f701c6ead3e086bf1e4a814
root <root@Favxlaw>
2024-02-24 08:04:48 +0100

    cors

--- backend/main.py
+++ backend/main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI 
 from routes import auth, courses, user 
+from fastapi.middleware.cors import CORSMiddleware
+
 #from schemas.user import User
 
 app = FastAPI(include_in_schema_for_serialization={"schema"})
 
 app = FastAPI()
+origins = [
+    "http://localhost",
+    "http://localhost:8000"
+    "http://localhost:9000"
+]
+
+app.add_middleware(CORSMiddleware, allow_credentials=True,
+                   allow_methods=["*"],
+                   allow_headers=["*"],
+                   allow_origins=origins)
 
 @app.get("/")
 async def root():
